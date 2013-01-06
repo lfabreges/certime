ff9b234d94166f3f1a5bcb7ebb1fa632133d171e
Ludovic Fabrèges <ludovic.fabreges@gmail.com>
2013-01-06 16:06:44 +0100

    Ajustements au niveau du codepad et suppression du pied-de-page.

--- public/js/codepad.js
+++ public/js/codepad.js
@@ -1,36 +1,51 @@
-$(document).ready(function () {
-    var codepadResult = $('#codepadResult');
-    var editor = ace.edit("codepadEditor");
+/*
+ * This file is part of Certime.
+ *
+ * Certime is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Certime is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Certime. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+$(document).ready(function() {
+    var codepadResult = $('#codepadResult'),
+        editor = ace.edit("codepadEditor");
     
     editor.setTheme("ace/theme/github");
     editor.setShowPrintMargin(false);
     editor.getSession().setMode("ace/mode/php");
     
-    editor.getSession().on(
-        'change',
-        function codepadEval() {
-            if (typeof codepadEval.ajaxRequest !== "undefined") {
-                return;
-            }
-            codepadEval.ajaxRequest = $.ajax({
-                url: "index.php?controller=codepad&action=eval",
-                data: {
-                    code: editor.getValue()
-                },
-                success: function (data) {
-                    if (data === "") {
-                        codepadResult.removeClass("well");
-                    } else {
-                        codepadResult.addClass("well");
-                    }
-                    codepadResult.html(data);
-                },
-                complete: function () {
-                    delete codepadEval.ajaxRequest;
-                }
-            });
+    var codepadEvalFunction = function()
+    {
+        if (typeof codepadEvalFunction.ajaxRequest !== "undefined") {
+            return;
         }
-    );
+        
+        codepadEvalFunction.ajaxRequest = $.ajax({
+            url: "index.php?controller=codepad&action=eval",
+            data: {
+                code: editor.getValue()
+            },
+            success: function(data) {
+                codepadResult.html(data);
+            },
+            complete: function() {
+                delete codepadEvalFunction.ajaxRequest;
+            }
+        });
+    }
+        
+    editor.getSession().on('change', codepadEvalFunction);
+    editor.getSession().on('paste', codepadEvalFunction);
     
     editor.focus();
+    editor.insert("<?php\n\n");
 });
